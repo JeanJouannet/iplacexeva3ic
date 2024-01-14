661ecb4d402d0d91366653e44fe81e80128af34d
jeanjouannet98 <jean.jouannet@r1vs.com>
2024-01-13 23:08:14 -0300

    asd

---
+++ src/main/java/com/example/iplacexeva3ic/Iplacexeva3icApplication.java
@@ -0,0 +1,13 @@
+package com.example.iplacexeva3ic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iplacexeva3icApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iplacexeva3icApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iplacexeva3ic/Iplacexeva3icApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iplacexeva3ic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iplacexeva3icApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
